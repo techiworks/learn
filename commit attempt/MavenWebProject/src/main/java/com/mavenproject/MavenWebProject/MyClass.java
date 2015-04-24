ff635c6aacc62a389cfc44a5cea19491dfc3327a
techi <techiworks@gmail.com>
2015-04-23 22:25:11 -0700

    Maven Web Project

---
+++ commit attempt/MavenWebProject/src/main/java/com/mavenproject/MavenWebProject/MyClass.java
@@ -0,0 +1,9 @@
+package com.mavenproject.MavenWebProject;
+
+import java.util.*;
+
+public class MyClass {
+	public static void main(String[] a) {
+		List<String> l = new ArrayList<String>();   //Generics are used only in java version 1.5 or later.
+	}
+}
